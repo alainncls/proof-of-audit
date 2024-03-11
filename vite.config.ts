59515bbfdf7ee1542013f6a1abbe0bde71e0d5b2
Alain Nicolas <alain.nicolas@consensys.net>
2024-03-11 18:02:50 +0100

    feat Add Google Analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginRadar } from "vite-plugin-radar";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        VitePluginRadar({
+            analytics: {
+                id: "G-SDPCWGQYK3",
+            },
+        }),
+    ],
 })
