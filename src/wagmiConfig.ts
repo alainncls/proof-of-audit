63521ae7cc57e9d9ff9c8b4d255e115476513761
Alain Nicolas <alain.nicolas@consensys.net>
2024-11-27 14:54:38 +0100

    chore: Update Verax SDK + Infura key

--- src/wagmiConfig.ts
+++ src/wagmiConfig.ts
@@ -1,7 +1,10 @@
 import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
 import { linea, lineaSepolia, mainnet } from 'wagmi/chains';
+import { http } from 'wagmi';
 
 export const walletConnectProjectId = '68b9b40fbc3c45a909f03f864745955e';
+const infuraApiKey: string = '2VbuXFYphoB468fyFPinOmis7o5';
+
 const metadata = {
   name: 'Proof of Audit',
   description: 'Issue attestation of audits',
@@ -11,6 +14,13 @@ const metadata = {
 const chains = [lineaSepolia, linea, mainnet] as const;
 export const wagmiConfig = defaultWagmiConfig({
   chains,
+  transports: {
+    [linea.id]: http(`https://linea-mainnet.infura.io/v3/${infuraApiKey}`),
+    [lineaSepolia.id]: http(
+      `https://linea-sepolia.infura.io/v3/${infuraApiKey}`,
+    ),
+    [mainnet.id]: http(`https://mainnet.infura.io/v3/${infuraApiKey}`),
+  },
   projectId: walletConnectProjectId,
   metadata,
   enableCoinbase: false,
