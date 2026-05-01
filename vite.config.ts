8a3267dce788c0314ddcbfb713eda26e5e506805
Alain Nicolas <alain.nicolas@consensys.net>
2026-05-01 20:35:25 +0200

    fix: remove stale analytics plugin config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePluginRadar } from 'vite-plugin-radar';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [
-      react(),
-      ...(env.VITE_GA_ID
-        ? [
-            VitePluginRadar({
-              analytics: {
-                id: env.VITE_GA_ID,
-              },
-            }),
-          ]
-        : []),
-    ],
-  };
+export default defineConfig({
+  plugins: [react()],
 });
